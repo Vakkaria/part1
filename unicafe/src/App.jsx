37a72fc52cefd414c0f9a4e23a44f5c82e876aaa
Danila <fuckthisemail@duck.com>
2024-01-11 17:24:09 +0300

    Update unicafe project

--- unicafe/src/App.jsx
+++ unicafe/src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
+import {useState} from 'react'
 
 const Header = props => <h1>{props.name}</h1>
 
 const Button = props => {
     return (
         <>
-            <button onClick={props.handleGoodClick}> Good </button>
-            <button onClick={props.handleNeutralClick}> Neutral </button>
-            <button onClick={props.handleBadClick}> Bad </button>
+            <button onClick={props.handleGoodClick}> Good</button>
+            <button onClick={props.handleNeutralClick}> Neutral</button>
+            <button onClick={props.handleBadClick}> Bad</button>
         </>
-    )}
+    )
+}
 
 const StatisticLine = props => {
     return (
@@ -17,31 +18,34 @@ const StatisticLine = props => {
             <td>{props.text}</td>
             <td>{props.value}</td>
         </tr>
-    )}
+    )
+}
 
 const Statistics = props => {
-    const { good, neutral, bad } = props
-    if (good || neutral || bad ) {
+    const {good, neutral, bad} = props
+    if (good || neutral || bad) {
         return (
             <table>
                 <tbody>
-                    <StatisticLine text='good' value={good} />
-                    <StatisticLine text='neutral' value={neutral} />
-                    <StatisticLine text='bad' value={bad} />
-                    <StatisticLine text='all' value={good + neutral + bad} />
-                    <StatisticLine text='average' value={(good - bad) / (good + bad + neutral)} />
-                    <StatisticLine text='posirive' value={100 * (good / (good + bad + neutral)) + ' %'} />
+                <StatisticLine text='good' value={good}/>
+                <StatisticLine text='neutral' value={neutral}/>
+                <StatisticLine text='bad' value={bad}/>
+                <StatisticLine text='all' value={good + neutral + bad}/>
+                <StatisticLine text='average' value={(good - bad) / (good + bad + neutral)}/>
+                <StatisticLine text='posirive' value={100 * (good / (good + bad + neutral)) + ' %'}/>
                 </tbody>
             </table>
-        )} else {
+        )
+    } else {
         return (
             <table>
                 <tbody>
-                    <StatisticLine text='No feedback given' />
+                <StatisticLine text='No feedback given'/>
                 </tbody>
             </table>
-        )}
+        )
     }
+}
 
 const App = () => {
     // save clicks of each button to its own state
@@ -53,7 +57,7 @@ const App = () => {
         <div>
             <Header name='give feedback' />
             <Button handleGoodClick={() => setGood(good + 1)} handleNeutralClick=
-                {() => setNeutral(neutral + 1)} setBadClick={() => setBad(bad + 1)} />
+                {() => setNeutral(neutral + 1)} handleBadClick={() => setBad(bad + 1)}/>
             <Header name='statistics' />
             <Statistics good={good} neutral={neutral} bad={bad} />
         </div>
